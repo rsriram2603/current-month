2831ae762007479f0b96563215c4e6aab50a985c
rsriram2603 <rsriram830@gmail.com>
2023-03-24 16:13:45 +0530

    new file add

---
+++ main.js
@@ -0,0 +1,14 @@
+	let a=new Date();
+	let res=a.getMonth();
+	if(res>=0 && res<=4)
+	{
+		console.log("summer");
+	}
+	else if(res>=5 && res<=8)
+	{
+		console.log("winter");
+	}
+	else if(res>=9 && res<=11)
+	{
+	console.log("fall");
+	}
